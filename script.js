db28abd7a7203f8009466c7e6da2c783808783fc
Gino Longobucco <gino_longob@hotmail.com>
2023-05-19 23:34:31 -0300

    Entrega final JS v1
    Se agrego data.json y fetch para cumplir con la consigna, se soluciono error causado por sweetalert al no estar su .min.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const nombreElement = document.querySelector(".nombre");
 const edadElement = document.querySelector(".edad");
 const fotoElement = document.querySelector(".foto");
@@ -7,75 +6,61 @@ const btnSi = document.querySelector(".si");
 const tablaResultados = document.querySelector(".tabla-resultados");
 const tbody = document.querySelector(".resultados");
 
-const personas = [
-  {
-    nombre: "Javi",
-    edad: 52,
-    foto: "./assets/javi.png",
-  },
-  {
-    nombre: "Horacio",
-    edad: 57,
-    foto: "./assets/larreta.png",
-  },
-  {
-    nombre: "Mauri",
-    edad: 64,
-    foto: "./assets/mauri.jpg",
-  },
-  {
-    nombre: "Máximo",
-    edad: 46,
-    foto: "./assets/maximo.jpg",
-  },
-  {
-    nombre: "Albert",
-    edad: 64,
-    foto: "./assets/albert.png",
-  },
-];
 let currentIndex = 0;
 let decisiones = {};
-function mostrarPersona() {
-  const personaActual = personas[currentIndex];
-  nombreElement.textContent = personaActual.nombre;
-  edadElement.textContent = personaActual.edad;
-  fotoElement.src = personaActual.foto;
+let personas;
+
+function mostrarPersona(persona) {
+  nombreElement.textContent = persona.nombre;
+  edadElement.textContent = persona.edad;
+  fotoElement.src = persona.foto;
 }
-mostrarPersona();
+
 function handleClick(decision) {
   const personaActual = personas[currentIndex];
   decisiones[personaActual.nombre] = decision;
   currentIndex++;
   if (currentIndex < personas.length) {
-    mostrarPersona();
+    mostrarPersona(personas[currentIndex]);
   } else {
     mostrarResultados();
   }
 }
-function mostrarResultados() {
-    tablaResultados.style.display = "block";
-    personas.forEach((persona) => {
-      const fila = document.createElement("tr");
-      const nombre = document.createElement("td");
-      const decision = document.createElement("td");
-      const match = document.createElement("td");
-      nombre.textContent = persona.nombre;
-      decision.textContent = decisiones[persona.nombre];
-      match.textContent = decisiones[persona.nombre] === "amor" ? "Sí" : "No";
-  
-      fila.appendChild(nombre);
-      fila.appendChild(decision);
-      fila.appendChild(match);
-      tbody.appendChild(fila);
-    });
-  
-    Swal.fire(
-      'No hay más candidatos por hoy',
-      'Revisa si tuviste algún match!',
-      'success'
-    );
-  }
 
-btnNo.addEventListener("click", () => handleClick("odio"));
-btnSi.addEventListener("click", () => handleClick("amor"));
+function mostrarResultados() {
+  tablaResultados.style.display = "block";
+  personas.forEach((persona) => {
+    const fila = document.createElement("tr");
+    const nombre = document.createElement("td");
+    const decision = document.createElement("td");
+    const match = document.createElement("td");
+    nombre.textContent = persona.nombre;
+    decision.textContent = decisiones[persona.nombre];
+    match.textContent =
+      decisiones[persona.nombre] === "amor" ? "Sí" : "No";
+
+    fila.appendChild(nombre);
+    fila.appendChild(decision);
+    fila.appendChild(match);
+    tbody.appendChild(fila);
+  });
+
+  Swal.fire(
+    'No hay más candidatos por hoy',
+    '¡Revisa si tuviste algún match!',
+    'success'
+  );
+}
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    personas = data;
+    mostrarPersona(personas[currentIndex]);
+
+    btnNo.addEventListener("click", () => handleClick("odio"));
+    btnSi.addEventListener("click", () => handleClick("amor"));
+  })
+  .catch(error => {
+    console.log('Error al cargar los datos:', error);
+  });
